002b3b5ac9df841723ba9cb6f62acb84552f602b
ahmedmdl <43715890+ahmedmdl@users.noreply.github.com>
2019-01-09 10:18:46 +0000

    Update Pi.py

--- Pi/Pi.py
+++ Pi/Pi.py
@@ -3,11 +3,18 @@ import time
 
 ROW_PINS = [18, 4]
 COL_PINS = [10, 9]
+ROW_No = 2
+COL_No = 2
 
-kp = GP.KeypadFactory().create_keypad(row=2 , col=2, row_pins= ROW_PINS, col_pins= COL_PINS, repeat= True, repeat_rate= 5, key_delay= 100)
+kp = GP.KeypadFactory().create_keypad(row=ROW_No , col=COL_No, row_pins= ROW_PINS, col_pins= COL_PINS, repeat= True, repeat_rate= 5, key_delay= 100)
+
 def printkey(key):
-    print(key)
+   print(key)
+   with open('Pi_dump.txt','w') as f:  
+     f.write(str(key))
+     
 kp.registerKeyPressHandler(printkey)
+
 try:
   while(True):
     time.sleep(0.2)
