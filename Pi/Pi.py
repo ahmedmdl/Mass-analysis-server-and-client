35084f12fb7fe296b89df7b1cc9d1b2112a75ba1
ahmedmdl <43715890+ahmedmdl@users.noreply.github.com>
2019-01-31 04:18:57 +0200

    Update Pi.py

--- Pi/Pi.py
+++ Pi/Pi.py
@@ -1,12 +1,12 @@
 import Gpio_factory as GP
 import time
 
-ROW_PINS = [18, 4, 3, 17, 24,25]
-COL_PINS = [10, 9, 2, 27, 23, 8]
-ROW_No = 2
-COL_No = 2
+ROW_PINS = [18, 4, 3]
+COL_PINS = [10, 9, 2]
+ROW_No = 3
+COL_No = 3
 
-kp = GP.KeypadFactory().create_keypad(row= 2 , col= 2, row_pins= [18, 4], col_pins= [10, 9], repeat= True, repeat_rate= 5, key_delay= 100)
+kp = GP.KeypadFactory().create_keypad(row= 3 , col= 3, row_pins= [18, 4, 3], col_pins= [10, 9, 2], repeat= True, repeat_rate= 5, key_delay= 100)
 
 def printkey(key):
    print(key,"key")
